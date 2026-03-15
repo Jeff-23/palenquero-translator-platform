b24bc8c232b4be3f24b49727aa5495a0a435b27b
jeffry-Salazar <jeffryalex14@gmail.com>
2026-03-14 20:23:46 -0500

    Creación del servidor con express, el servidor tiene la contante o instancia app

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
+const express = require("express"); // Importamos el framework Express para crear el servidor web
+const app = express(); // Creamos el servidor en express y lo asignamos a la variable app
+app.use(express.json());// Middleware para permitir texto en formato JSON   
 
+app.get("/health", (req, res) => {
+    res.json({ 
+        status: "Corriendo", 
+        message : "backend funcionando correctamente"
+    });
+    });// Creamos una ruta que muestre el estado del servidor y un mensaje indicando que el backend está funcionando correctamente
+
+    const PORT = 4000; // Puerto asignado para el servidor
+
+    app.listen(PORT, () => {
+        console.log("servidor corriendo en puerto ${PORT}");
+    }); // El servidor escucha el puerto y envia un mensaje a la consola indicando que el servidor está corriendo en el puerto que se le asignó
